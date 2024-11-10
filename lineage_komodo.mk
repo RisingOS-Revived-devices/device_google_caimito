7a695183c2b7e2cd63622df09f50d6c5db804d11
Alvin Francis <nivlafx@gmail.com>
2024-11-09 21:27:02 -0400

    caimito: Update risingOS flags
     * Relocated some flags under risingOS header for all devices
    
    Change-Id: I89b30d00b18fc75c88c7f6746fcc48067fe804ec
    Signed-off-by: Alvin Francis <nivlafx@gmail.com>

--- lineage_komodo.mk
+++ lineage_komodo.mk
@@ -21,14 +21,14 @@ PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 9 Pro XL
 PRODUCT_NAME := lineage_$(DEVICE_CODENAME)
 
-# Addons
+# RisingOS
+RISING_MAINTAINER="Lord Itachi|Niv"
+TARGET_DEFAULT_PIXEL_LAUNCHER := true
+TARGET_ENABLE_BLUR := true
 TARGET_HAS_UDFPS := true
-
-# Gms
-WITH_GMS := true
-
-# Ship Google Camera
 TARGET_PREBUILT_GOOGLE_CAMERA := true
+TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := true
+WITH_GMS := true
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2992
@@ -38,5 +38,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="komodo-user 15 BP1A.250405.007 13240079 release-keys" \
     BuildFingerprint=google/komodo/komodo:15/BP1A.250405.007/13240079:user/release-keys \
     DeviceProduct=$(DEVICE_CODENAME)
+    RisingChipset="Tensor G4" \
+    RisingMaintainer="Slogar25"
 
 $(call inherit-product, $(VENDOR_PATH)/$(DEVICE_CODENAME)-vendor.mk)
